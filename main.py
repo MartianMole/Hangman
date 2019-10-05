3437a96fbba6d30380957d28caa8361446e6f4a1
Zakharov Daniil <danka3819@gmail.com>
2019-10-05 13:06:52 +0300

    Just little scratch in console

--- main.py
+++ main.py
@@ -1,1 +1,44 @@
-print(1)123
+import random
+
+
+def writeCurWord(word):
+    print(*word)
+
+
+def putLetter(letter, word):
+    global xWord
+    for i in range(len(word)):
+        if xWord[i] == letter:
+            word[i] = letter
+    return word
+
+
+lifeCount = 5
+xWord = [i for i in 'apple']
+print(xWord)
+curWord = ['_'] * 5
+usedLetters = []
+
+
+def mainLoop():
+    global lifeCount
+    writeCurWord(curWord)
+    if '_' not in curWord:
+        print('You win')
+        exit()
+    else:
+        guessLetter = input()
+        if guessLetter in xWord:
+            putLetter(guessLetter, curWord)
+        else:
+            lifeCount -= 1
+            if lifeCount >= 0:
+                print(lifeCount, 'lifes remain')
+            else:
+                print(*xWord)
+                print('You loose(')
+                exit(0)
+    mainLoop()
+
+
+mainLoop()
